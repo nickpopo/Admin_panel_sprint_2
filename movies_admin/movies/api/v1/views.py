24aa4b2f3bb1ce6a3a5885d942fcb487b676e3e0
Nikolay Popov <koyabica@gmail.com>
2020-11-30 23:01:53 +0300

    1. remove verbosity in queryset. 2. fixes the problem when the application was just deployed and the user launches manage.py and get error that Q class can't resolve the prolem with Career.get_career_id cause in db is not yet any career and even no tables. (TODO)

--- movies_admin/movies/api/v1/views.py
+++ movies_admin/movies/api/v1/views.py
@@ -1,21 +1,31 @@
+import logging
+
+import django.db.utils
+from django.conf import settings
 from django.contrib.postgres.aggregates import ArrayAgg
-from django.db.models import Q, F
+from django.db.models import F, Q
 from django.http import JsonResponse
-from django.views.generic.list import BaseListView
 from django.views.generic.detail import BaseDetailView
-from movies.models import Filmwork, Career, CareerNameEnum
+from django.views.generic.list import BaseListView
+from movies.models import Career, CareerNameEnum, Filmwork
+
+logger = logging.getLogger()
 
 
 class MoviesApiMixin:
     model = Filmwork
     http_method_names = ['get']  # Список методов, которые реализует обработчик
-
-    actor = Q(personfilmwork__role=Career.get_career_id(
-        CareerNameEnum.ACTOR.value))
-    writer = Q(personfilmwork__role=Career.get_career_id(
-        CareerNameEnum.WRITER.value))
-    director = Q(personfilmwork__role=Career.get_career_id(
-        CareerNameEnum.DIRECTOR.value))
+    
+    actor = writer = director = None
+    try:
+        actor = Q(personfilmwork__role=Career.get_career_id(
+            CareerNameEnum.ACTOR))
+        writer = Q(personfilmwork__role=Career.get_career_id(
+            CareerNameEnum.WRITER))
+        director = Q(personfilmwork__role=Career.get_career_id(
+            CareerNameEnum.DIRECTOR))
+    except (django.db.utils.ProgrammingError, Career.DoesNotExist) as error:
+        logger.error(error)
 
     selected_fields = (
                         'id',
@@ -25,38 +35,41 @@ class MoviesApiMixin:
                         'rating',
                         )
 
-    queryset = Filmwork.objects.prefetch_related('genres', 'persons').values(*selected_fields).annotate(
-        type=F('type__name')).annotate(
-        genres=ArrayAgg('genres__name', distinct=True)).annotate(
-        actors=ArrayAgg('persons__full_name', filter=actor, distinct=True)).annotate(
-        writers=ArrayAgg('persons__full_name', filter=writer, distinct=True)).annotate(
-        directors=ArrayAgg('persons__full_name', filter=director, distinct=True))
+    queryset = Filmwork.objects.values(*selected_fields).annotate(
+        type=F('type__name'),
+        genres=ArrayAgg('genres__name', distinct=True),
+        actors=ArrayAgg('persons__full_name', filter=actor, distinct=True),
+        writers=ArrayAgg('persons__full_name', filter=writer, distinct=True),
+        directors=ArrayAgg('persons__full_name', filter=director, distinct=True),
+        )
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
 
 
 class MoviesListApi(MoviesApiMixin, BaseListView):
-    paginate_by = 50
+    paginate_by = settings.DJANGO_ITEMS_PER_PAGE
+    ordering = 'title'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
         page_size = self.get_paginate_by(queryset)
 
         if page_size:
-            paginator, page, queryset, is_paginated = self.paginate_queryset(
+            paginator, page, queryset, _ = self.paginate_queryset(
                 queryset, page_size)
             context = {
                 'count': paginator.count,
                 'total_pages': paginator.num_pages,
-                'prev': page.number - 1 if page.number > 1 else None,
-                'next': page.number + 1 if page.number + 1 < paginator.num_pages else None,
-                'results': list(queryset)
+                'prev': page.previous_page_number() if page.has_previous() else None,
+                'next': page.next_page_number() if page.has_next else None,
+                'results': queryset
             }
 
         return context
 
     def render_to_response(self, context, **response_kwargs):
+        context['results'] = list(context['results'])
         return JsonResponse(context)
 
 
